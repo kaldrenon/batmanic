6a169b4a061342031b39c872bc78ed8ed4ad9567
Andrew Fallows <kaldrenon@gmail.com>
2014-03-27 15:12:46 -0400

    create empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Batmanic::Application.config.secret_key_base = '14be969c4a356e1b3d285cce2e75a8c50b3a390a26ad11844424d48ef88760fb85eb3f1ef635a0476d6b3ce814deaa0a3c7ec9c32549bc1fa6744506396c054b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Batmanic::Application.config.session_store :cookie_store, key: '_batmanic_session'
